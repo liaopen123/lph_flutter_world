df697080cd12c42aa1caf8a5d0decaf79c06175a
廖鹏辉 <467969@qq.com>
2019-06-25 11:33:10 +0800

    viewGroup has complete

--- lib/viewgroup/StackDemo.dart
+++ lib/viewgroup/StackDemo.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 
-class StackDemo  extends StatelessWidget{
+class StackDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("层叠布局"),),
-        body:
-
-        ConstrainedBox(constraints: BoxConstraints.expand(),
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Container(
-              child: Text("hello world",style: TextStyle(color: Colors.white)),
-              color:  Colors.red,
-            ),
-            Positioned(
-              left: 18,
-              child: Text("我是爱你的辉仔"),
-            ),
-            Positioned(
-              top: 18,
-              child: Text("特别的爱你~"),
-            ),
-
-          ],
-
-
-        ),),
+        appBar: AppBar(
+          title: Text("层叠布局"),
+        ),
+        body: ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: Stack(
+            alignment: Alignment.center,
+            fit: StackFit.expand, //未定位widget占满Stack整个空间
+            children: <Widget>[
+              Positioned(
+                left: 18,
+                child: Text("我是爱你的辉仔"),
+              ),
+              Container(
+                child:
+                    Text("hello world", style: TextStyle(color: Colors.white)),
+                color: Colors.red,
+              ),
+              Positioned(
+                top: 18,
+                child: Text("特别的爱你~"),
+              ),
+            ],
+          ),
+        ),
       ),
-
     );
   }
-
 }
