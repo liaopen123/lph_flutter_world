3c1016132b572f8d7e41ffaeffea3b729c412e82
廖鹏辉 <467969@qq.com>
2019-11-22 16:20:44 +0800

    add svg

--- lib/classtwo/RecipeDemo.dart
+++ lib/classtwo/RecipeDemo.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lph_flutter_world/classtwo/row_and_column/RatingView.dart';
 
 class RecipeDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SvgPicture close = new SvgPicture.asset(
+      "images/yungu.svg",
+    );
+
     Widget title = Text(
       "Starwberry Pavlova",
       style: TextStyle(fontWeight: FontWeight.w800, fontSize: 36),
     );
 
-    Widget description = Container(
-      padding: EdgeInsets.symmetric(vertical: 10),
-        child:Text("""
-       如果说了后悔 是不是一切就能倒退
-　　回忆多么美 活着多么狼狈
-　　为什么这个世界 总要叫人尝伤悲
-　　我不能了解 也不想了解
-　　我好想好想飞 逃离这个疯狂世界
-　　那么多苦那么多累 那么多莫名的泪水
-　　我好想好想飞 逃离这个疯狂的世界
-　　如果是你发现了我 也别将我挽回
-　　想了你一整夜 再也想不起你的脸
-　　你是一种感觉 写在夏夜晚风里面""")
-    );
+    Widget description =
+        Container(padding: EdgeInsets.symmetric(vertical: 10), child:   SizedBox(
+          width: 100,
+          height: 100,
+          child: close,
+        ));
 
     Widget timeBlockItem(IconData icon, String des, String time) {
       return Column(
         children: <Widget>[
+
           Icon(
             icon,
             color: Colors.green,
@@ -65,26 +63,25 @@ class RecipeDemo extends StatelessWidget {
         body: Row(
           children: <Widget>[
             Expanded(
-            flex: 1,
+              flex: 1,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-
                   title,
                   description,
                   RatingView(),
                   TimeSection(),
                 ],
               ),
-        ),
-            Expanded(
-
-              flex: 2,
-            child:  Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Image.asset("images/pic1.jpg", ),
             ),
-
+            Expanded(
+              flex: 2,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Image.asset(
+                  "images/pic1.jpg",
+                ),
+              ),
             ),
           ],
         ),
